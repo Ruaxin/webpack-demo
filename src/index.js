75dbe70d68a10a52757b8a07c295c20b8ffc95b2
ruaxin <qy.xin@foxmail.com>
2020-01-01 11:20:35 +0800

    css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './x.js'
 console.log('hi')
 console.log('hello')
 console.log('world')
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'My App',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }]
     }
 };
